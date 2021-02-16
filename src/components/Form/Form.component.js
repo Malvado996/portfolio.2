a8dc9a1a93a70bce55c4b3e50a26d24f75bfb78e
Malvado996 <malvado996@gmail.com>
2021-02-15 21:33:25 -0500

    added images and content to contact page

---
+++ src/components/Form/Form.component.js
@@ -0,0 +1,21 @@
+import React from 'react'; 
+
+import './Form.styles.scss';
+
+const Form = () => {
+
+  return(
+    <div className="form-container">
+      <h3>Contact Me</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+        laboris nisi ut aliquip ex ea commodo consequat.
+      </p>
+      <p>Email:<a href="mailto: philip.rodrigues.js@gmail.com">philip.rodrigues.js@gmail.com</a></p>
+    </div>
+  )
+
+}
+
+export default Form
--- src/pages/Contact/Contact.component.js
+++ src/pages/Contact/Contact.component.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
+import Form from '../../components/Form/Form.component';
+import empireState from '../../assets/images/empireState.svg';
+import chrysler from '../../assets/images/chrysler.svg';
+
 import './Contact.styles.scss';
 
 const Contact = () => {
 
     return(
         <div className="contact-container">
-
+            <img src={ empireState } className="empireState" alt="logo" />
+            <Form />
+            <img src={ chrysler } className="chrysler" alt="logo" />
         </div>
     )
 }
