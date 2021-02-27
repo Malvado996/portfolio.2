293ea10234352b34df148ad3bb823122d21180a0
Malvado996 <malvado996@gmail.com>
2021-02-27 17:27:28 -0500

    added form element

--- src/components/Form/Form.component.js
+++ src/components/Form/Form.component.js
@@ -6,13 +6,26 @@ const Form = () => {
 
   return(
     <div className="form-container">
+      
       <h3>Contact Me</h3>
+
+      <form>
+        <label for='name'>Name:</label><br></br>
+        <input type='text' id='name' name='name'></input><br></br>
+        <label for='email'>email:</label><br></br>
+        <input type='email' id='email' name='email'></input><br></br>
+        <label for='message'>message:</label><br></br>
+        <textarea id='message' name='message' rows='5' cols='30'></textarea>
+      </form>
+      
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
         Ut enim ad minim veniam, quis nostrud exercitation ullamco 
         laboris nisi ut aliquip ex ea commodo consequat.
       </p>
-      <p>Email:<a href="mailto: philip.rodrigues.js@gmail.com"> philip.rodrigues.js@gmail.com</a></p>
+      
+      <span>Email:<a href="mailto: philip.rodrigues.js@gmail.com"> philip.rodrigues.js@gmail.com</a></span>
+
     </div>
   )
 
