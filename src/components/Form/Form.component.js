b2a9e46a9fa92ed9924790e7262e508e1dcadd7f
Malvado996 <malvado996@gmail.com>
2021-02-27 18:23:25 -0500

    refactoring/css for form element

--- src/components/Form/Form.component.js
+++ src/components/Form/Form.component.js
@@ -9,22 +9,22 @@ const Form = () => {
       
       <h3>Contact Me</h3>
 
-      <form>
-        <label for='name'>Name:</label><br></br>
-        <input type='text' id='name' name='name'></input><br></br>
-        <label for='email'>email:</label><br></br>
-        <input type='email' id='email' name='email'></input><br></br>
-        <label for='message'>message:</label><br></br>
-        <textarea id='message' name='message' rows='5' cols='30'></textarea>
-      </form>
-      
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
         Ut enim ad minim veniam, quis nostrud exercitation ullamco 
         laboris nisi ut aliquip ex ea commodo consequat.
       </p>
+
+      <form>
+        <label for='name'><p className='form-label-text'>Name:</p></label>
+        <input type='text' id='name' name='name'></input>
+        <label for='email'><p className='form-label-text'>Email:</p></label>
+        <input type='email' id='email' name='email'></input>
+        <label for='message'><p className='form-label-text'>Message:</p></label>
+        <textarea id='message' name='message' rows='5' cols='30'></textarea>
+      </form>
       
-      <span>Email:<a href="mailto: philip.rodrigues.js@gmail.com"> philip.rodrigues.js@gmail.com</a></span>
+      <span className='email-span'>Email:<a href="mailto: philip.rodrigues.js@gmail.com"> philip.rodrigues.js@gmail.com</a></span>
 
     </div>
   )
